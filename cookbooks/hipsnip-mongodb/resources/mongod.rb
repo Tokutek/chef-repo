080c65fed560611795d6a367573d3dcf22b7b077
John Esmet <john.esmet@gmail.com>
2014-03-20 19:20:52 +0000

    Remove hipsnip-mongodb from this repo - will be managed separately in the Tokutek/hipsnip-mongodb repo

--- cookbooks/hipsnip-mongodb/resources/mongod.rb
+++
@@ -1,6 +0,0 @@
-actions :create
-default_action :create
-
-attribute :bind_ip, :kind_of => String, :default => ''
-attribute :port, :kind_of => Integer, :default => 27017
-attribute :replica_set, :kind_of => String, :default => ''
--- cookbooks/hipsnip-mongodb/resources/replica_set.rb
+++
@@ -1,5 +0,0 @@
-actions :create
-default_action :create
-
-attribute :replica_set, :kind_of => String, :name_attribute => true
-attribute :members, :kind_of => Array, :required => true
