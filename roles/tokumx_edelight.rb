6a30a036e6f52d10712a24956fbac1694e3c42af
John Esmet <john.esmet@gmail.com>
2014-03-25 03:09:25 +0000

    Add roles for edelight-based replica members and stand alone instances

---
+++ roles/tokumx_edelight.rb
@@ -0,0 +1,24 @@
+name "tokumx_edelight"
+description "tokumx stand-alone server"
+
+override_attributes(
+  "mongodb" => {
+    "user" => "tokumx",
+    "group" => "tokumx",
+    "package_name" => "tokumx",
+    "syslog" => "false",
+    "config" => {
+      "dbpath" => "/mnt/tokumx_data",
+      "logpath" => "/var/log/tokumx/tokumx.log",
+    },
+    "dbconfig_file" => "/etc/tokumx.conf",
+    "instance_name" => "tokumx",
+    "default_init_name" => "tokumx"
+  }
+)
+
+run_list(
+  "recipe[apt]",
+  "recipe[mongodb::tokutek_repo]",
+  "recipe[mongodb::default]"
+)
---
+++ roles/tokumx_replicaset_edelight.rb
@@ -0,0 +1,27 @@
+name "tokumx_replicaset_edelight"
+description "tokumx replica-set member"
+
+override_attributes(
+  "mongodb" => {
+    "user" => "tokumx",
+    "group" => "tokumx",
+    "package_name" => "tokumx",
+    "syslog" => "false",
+    "config" => {
+      "dbpath" => "/mnt/tokumx_data",
+      "logpath" => "/var/log/tokumx/tokumx.log",
+      "replSet" => "replicaset0"
+    },
+    "dbconfig_file" => "/etc/tokumx.conf",
+    "cluster_name" => "cluster0",
+    "shard_name" => "shard0",
+    "instance_name" => "tokumx",
+    "default_init_name" => "tokumx"
+  }
+)
+
+run_list(
+  "recipe[apt]",
+  "recipe[mongodb::tokutek_repo]",
+  "recipe[mongodb::replicaset]"
+)
