0bc64b244c2c5848a56496950ac5e1fd6f0376b9
John Esmet <john.esmet@gmail.com>
2014-03-28 21:27:47 +0000

    Fix vanilla mongodb replica set recipe

--- roles/mongodb_replicaset.rb
+++ roles/mongodb_replicaset.rb
@@ -2,5 +2,5 @@ name "mongodb_replicaset"
 description "stand-alone vanilla mongodb instance"
 run_list(
     "recipe[apt]",
-    "recipe[hipsnip-mongodb::replica_set]"
+    "recipe[mongodb::replicaset]"
 )
