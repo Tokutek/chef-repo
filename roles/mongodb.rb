eed29848b6107974c0b6b6ef0a90c52f2fb507b5
John Esmet <john.esmet@gmail.com>
2014-04-08 23:41:57 +0000

    Remove scripts and roles - no longer needed now that Batali is implemented far enough to provide this basic functionality

--- roles/mongodb.rb
+++
@@ -1,7 +0,0 @@
-name "mongodb"
-description "stand-alone vanilla mongodb instance"
-run_list(
-    "recipe[mongodb::10gen_repo]",
-    "recipe[apt]",
-    "recipe[mongodb]"
-)
--- roles/mongodb_replicaset.rb
+++
@@ -1,6 +0,0 @@
-name "mongodb_replicaset"
-description "stand-alone vanilla mongodb instance"
-run_list(
-    "recipe[apt]",
-    "recipe[mongodb::replicaset]"
-)
